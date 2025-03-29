d5f721c153be4c7739996170ef8c7c099b448ae1
Voicu Liviu <bobdenaut@users.noreply.github.com>
2025-03-30 00:14:50 +0200

    Update freebsd-install.sh

--- freebsd-install.sh
+++ freebsd-install.sh
@@ -48,6 +48,40 @@ geli attach gpt/zdata1
 gmirror label efi gpt/efi0 gpt/efi1
 gmirror load
 gmirror status
+#
+# The following taked from FreeBSD repo code is showing how datasets are created by the installer:
+# https://svnweb.freebsd.org/base/release/11.2.0/usr.sbin/bsdinstall/scripts/zfsboot?revision=335563&view=markup
+# Credit goes to FreeBSD ofcourse!
+#
+# Default ZFS datasets for root zpool
+# Boot Environment [BE] root and default boot dataset
+#/$ZFSBOOT_BEROOT_NAME                           mountpoint=none
+#/$ZFSBOOT_BEROOT_NAME/$ZFSBOOT_BOOTFS_NAME      mountpoint=/
+#
+# Compress /tmp, allow exec but not setuid
+#/tmp            mountpoint=/tmp,exec=on,setuid=off
+#
+# Don't mount /usr so that 'base' files go to the BEROOT
+#/usr            mountpoint=/usr,canmount=off
+# 	
+# Home directories separated so they are common to all BEs
+#/usr/home       # NB: /home is a symlink to /usr/home
+# 	
+# Ports tree
+#/usr/ports      setuid=off
+# 	
+# Source tree (compressed)
+#/usr/src
+# 	
+# Create /var and friends
+#/var            mountpoint=/var,canmount=off
+#/var/audit      exec=off,setuid=off
+#/var/crash      exec=off,setuid=off
+#/var/log        exec=off,setuid=off
+#/var/mail       atime=on
+#/var/tmp        setuid=off
+
+
 #zroot creation
 zpool create -f -o ashift=12 \
  -O compression=lz4 \
